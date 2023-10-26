cc95ce2a63ecb0536f4954b2aedaec4415e0c3a6
C1326348 Hyago Souza <C1326348@interno.bb.com.br>
2023-10-26 18:57:52 -0300

    cria metodo findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,18 @@
 package application;
 
-import java.util.Date;
-
-import model.entitites.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entitites.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department d1 = new Department(1,"Books");
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(3);
 		
-		Seller s1 = new Seller(21, "Bob", "bob@gmail.com", new Date(),3000.0, d1);
+		System.out.println(seller);
 		
-		System.out.println(s1);
 	}
 
 }
