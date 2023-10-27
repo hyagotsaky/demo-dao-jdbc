cf75697f8f7de2c31e659d45220889cb28ffcec3
C1326348 Hyago Souza <C1326348@interno.bb.com.br>
2023-10-26 21:38:31 -0300

    cria métodos findByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entitites.Department;
 import model.entitites.Seller;
 
 public class Program {
@@ -12,8 +15,20 @@ public class Program {
 		Seller seller = sellerDao.findById(3);
 		System.out.println("--------------FindById");
 		System.out.println(seller);
+		
+		
+		System.out.println("--------------FindByDepartment");
+		Department department = new Department(2,null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		list.forEach(System.out::println);
+
 		System.out.println("--------------");
 		
+		
+		
+
+		
+
 	}
 
 }
